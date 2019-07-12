07fe88c69fb21312ce8bf150d91329bbe9f1336b
TaniaAG <38076829+TaniaAG@users.noreply.github.com>
2019-07-12 20:36:45 +0200

    - partial solution - random groups in the cart added

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <semaphore.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 sem_t groupSem, singleSem;
 
@@ -14,7 +15,7 @@ int semValue(sem_t semaphore){
 
 void *groupQueue(void *ptr){
     sem_post(&groupSem);
-    sleep(1);
+    //sleep(1);
 
 }
 
@@ -25,11 +26,34 @@ void *singleQueue(void *ptr){
 }
 
 void *rolerCoaster(void *ptr) {
-    if (semValue(&groupSem) > 0) {
-        printf("%d ", semValue(groupSem));
-        
+    int array[3];
+    int group = 0;
+    int x = 0;
+        if(semValue(groupSem) > 0 && semValue(singleSem) > 0) {
+            //printf("%d ", semValue(groupSem));
+            while(x < 10) {
+                x++;
+                for(int i = 0; i < 3; i++) {
+                    sleep(1);
+                    array[i] = rand() % 2 + 2;
+                    //printf("%d", array[i]);
+                }
+                if(array[0] == 3 && array[1] == 3){
+                    sem_wait(&groupSem);
+                    printf("**************\nThis cart left.\nIn the cart there are: %d - %d\nthere are %d groups in the queue.", array[0], array[1], semValue(groupSem));
+                }
+            }
 
-    }
+            /*for(int i = 0; i< 3; i++){
+
+                array[i] = rand() %2 +2;
+                printf("%d", array[i]);
+            }*/
+
+
+        }
+
+
 }
 
 
@@ -39,8 +63,8 @@ int main() {
     int targ[3];
     pthread_t thread[3];
     int i = 0;
-    while(i< 100) {
-        i++;
+    while(i< 10) {
+       i++;
         pthread_create(&thread[0], NULL, &groupQueue, (void *) &targ[0]);
         pthread_create(&thread[1], NULL, &singleQueue, (void *) &targ[1]);
         pthread_create(&thread[2], NULL, &rolerCoaster, (void *) &targ[2]);
